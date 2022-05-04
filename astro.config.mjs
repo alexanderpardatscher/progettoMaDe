81f7a116eccfc1b4a5689a280e2076aabe9a0126
guitar-gigio <guitar.gigi69@gmail.com>
2022-05-04 13:17:41 +0200

    rimosso tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,4 @@
 import { defineConfig } from 'astro/config';
-import tailwindConfig from './tailwind.config';
 
 // https://astro.build/config
 export default defineConfig({});
-
-devOptions: {
-    tailwindConfig: './tailwind.config.js';
-}
--- tailwind.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  content: ["./src/**/*.{astro,js,jsx,ts,tsx,vue}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
